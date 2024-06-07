bde8b2ca00b215a4b6d387dfee6a54d826829313
Haritony <hei.haritony@gmail.com>
2024-06-07 23:07:48 +0300

    feat:end of login

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-  event.preventDefault(); 
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  const name = document.getElementById('name').value.trim();
-  const dob = document.getElementById('dob').value.trim();
-  const city = document.getElementById('city').value.trim();
-  const email = document.getElementById('email').value.trim();
-  const password = document.getElementById('password').value.trim();
-  const genderMale = document.getElementById('male').checked;
-  const genderFemale = document.getElementById('female').checked;
+    const loadingElement = document.getElementById("loading");
+    loadingElement.style.display = "flex";
 
-  if (name && dob && city && email && password && (genderMale || genderFemale)) {
-    window.location.href = 'main.html'; 
-  } else {
-    alert('Please fill in all fields.');
-  }
-});
+    const name = document.getElementById("name").value.trim();
+    const dob = document.getElementById("dob").value.trim();
+    const city = document.getElementById("city").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const genderMale = document.getElementById("male").checked;
+    const genderFemale = document.getElementById("female").checked;
+
+    setTimeout(() => {
+      if (
+        name &&
+        dob &&
+        city &&
+        email &&
+        password &&
+        (genderMale || genderFemale)
+      ) {
+        window.location.href = "main.html";
+      } else {
+        alert("Please fill in all fields.");
+        loadingElement.style.display = "none";
+      }
+    }, 2000);
+  });
 
 function cancel() {
-  document.getElementById('loginForm').reset();
+  document.getElementById("loginForm").reset();
 }
